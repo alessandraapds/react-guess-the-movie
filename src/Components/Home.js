312ff98791022e67bea4dd7edf416f0dcfd9c97c
Alessandra Silva <alessandra.apds@gmail.com>
2023-03-23 19:10:23 +0100

    app creation

---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-6">Test</div>
+          <div class="col-6">Test 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/fetchData.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const fetchData = () => {
+  return <div>fetchData</div>;
+};
+
+export default fetchData;
